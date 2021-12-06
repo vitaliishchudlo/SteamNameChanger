f70bd3381728601f2c529d72290e19ffe5356ee2
vitaliishchudlo <vitalik.shchudlo01@ukr.net>
2021-12-06 02:02:46 +0200

    Updated the project structure to a functional structure. Version 2.0

--- app.py
+++ app.py
@@ -1,50 +1,96 @@
+import os
+import random
 import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(executable_path='PATH')
+path_to_webdriver = os.getcwd().replace('\\\\', '\\') + '\\chromedriver.exe'
 
-driver.get('https://store.steampowered.com/login/')
+driver = webdriver.Chrome(executable_path=path_to_webdriver)
 
-time.sleep(3)
+nicknames_for_change = []
 
-login_input_line = driver.find_element(By.NAME, 'username')
-password_input_line = driver.find_element(By.NAME, 'password')
+last_nickname = ''
 
-login_input_line.send_keys('LOGIN')
-password_input_line.send_keys('PASSWORD')
 
-time.sleep(1)
 
-driver.find_element_by_css_selector('.btn_blue_steamui.btn_medium.login_btn').click()
 
-STEAM_GUARD_CODE = input('Plase, input your steam guard code here: ').upper()
-print('\n\n\n   YOUR STEAM GUARD CODE: ', STEAM_GUARD_CODE, '\n\n\n')
-steam_guard_input_line = driver.find_element(By.ID, 'twofactorcode_entry')
-steam_guard_input_line.send_keys(STEAM_GUARD_CODE)
+def steam_loging():
+    driver.get('https://store.steampowered.com/login/')
+    time.sleep(3)
 
-time.sleep(1)
+    login_input_line = driver.find_element(By.NAME, 'username')
+    password_input_line = driver.find_element(By.NAME, 'password')
 
-driver.find_element_by_xpath('//*[@id="login_twofactorauth_buttonset_entercode"]/div[1]/div[1]').click()
+    login_input_line.send_keys(input('Enter your steam login: '))
+    password_input_line.send_keys(input('Enter your steam password: '))
 
-time.sleep(3)
+    time.sleep(1)
 
-STEAM_PROFILE_LINK = 'https://steamcommunity.com/id/YOUID/'
+    driver.find_element_by_css_selector('.btn_blue_steamui.btn_medium.login_btn').click()
 
-driver.get(f'{STEAM_PROFILE_LINK}edit/info/')
+    STEAM_GUARD_CODE = input('Please, input your steam guard code here: ').upper()
 
-time.sleep(3)
+    print('\n\n\n   YOUR STEAM GUARD CODE: ', STEAM_GUARD_CODE, '\n\n\n')
 
-new_name_input_line = driver.find_element(By.NAME, 'personaName')
+    steam_guard_input_line = driver.find_element(By.ID, 'twofactorcode_entry')
+    steam_guard_input_line.send_keys(STEAM_GUARD_CODE)
 
-new_name_input_line.clear()
-new_name_input_line.send_keys('ilonka')
+    driver.find_element_by_xpath('//*[@id="login_twofactorauth_buttonset_entercode"]/div[1]/div[1]').click()
 
-time.sleep(1)
+    time.sleep(3)
 
-driver.find_element_by_xpath('//*[@id="application_root"]/div[2]/div[2]/form/div[7]/button[1]').click()
 
-time.sleep(10)
+def get_steam_editprofile_page():
+    STEAM_PROFILE_LINK = input('Enter your profile link: ')  
+    driver.get(f'{STEAM_PROFILE_LINK}edit/info/')
+
+    time.sleep(3)
+
+
+def get_nicknames_from_user():
+    choice = input(f'Your list with nicknames: {nicknames_for_change}\n Your want add more?\n\n1 - Yes\n2 - No\n>>> ')
+    if choice == '1':
+        new_nickname = input('Enter nickname: ')
+        nicknames_for_change.append(new_nickname)
+        get_nicknames_from_user()
+
+
+def chose_nickname():
+    global last_nickname
+
+    nickname = random.choice(nicknames_for_change)
+    if nickname == last_nickname:
+        chose_nickname()
+    last_nickname = nickname
+    return nickname
+
+
+
+def change_steam_name():
+    new_name_input_line = driver.find_element(By.NAME, 'personaName')
+    new_name_input_line.clear()
+    nickname = chose_nickname()
+
+    new_name_input_line.send_keys(nickname)
+
+    time.sleep(1)
+
+    driver.find_element_by_xpath('//*[@id="application_root"]/div[2]/div[2]/form/div[7]/button[1]').click()
+
+    time.sleep(31)
+    change_steam_name()
+
+
+def start():
+    steam_loging()
+    get_steam_editprofile_page()
+    get_nicknames_from_user()
+    change_steam_name()
+
+
+start()
+
 driver.close()
 driver.quit()
