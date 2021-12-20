9ee6ba4028331228523819e6be89b8f6cce65a19
vitaliishchudlo <vitalik.shchudlo01@ukr.net>
2021-12-20 21:08:53 +0200

    Presaving before new structure of project. Need to recode all files

--- api_steam/authorize.py
+++ api_steam/authorize.py
@@ -2,20 +2,59 @@ import os
 
 from selenium.webdriver.common.by import By
 
-from data import USER_LOGIN, USER_PASSWORD, STEAM_AUTHORIZATION_PAGE
+from data import STEAM_AUTHORIZATION_PAGE, get_user_login, get_user_password
+
+import time
+import sys
 
 
 def authorize_user(driver):
     username_form_field = driver.find_element(By.NAME, 'username')
     password_form_field = driver.find_element(By.NAME, 'password')
 
-    username_form_field.send_keys(USER_LOGIN)
-    password_form_field.send_keys(USER_PASSWORD)
+    username_form_field.send_keys(get_user_login())
+    password_form_field.send_keys(get_user_password())
 
     driver.find_element(By.CSS_SELECTOR, '.btn_blue_steamui.btn_medium.login_btn').click()
 
     os.system('cls')
+    time.sleep(3)
+    # НИЩЕ ПЕРЕВІРКА ЧИ ТРУ ДАНІ ДЛЯ ВХОДУ
+    if driver.find_element(By.CLASS_NAME, 'newmodal'):
+        STEAM_GUARD_CODE = input('Please, input your steam guard code here: ').upper()
+        print('\n\n\n   YOUR STEAM GUARD CODE: ', STEAM_GUARD_CODE, '\n\n\n')
 
-    if driver.current_url == STEAM_AUTHORIZATION_PAGE:
-        return False
-    return True
+        try:
+            # Mobile (SteamGuard) authorizing
+            steam_guard_input_line = driver.find_element(By.ID, 'twofactorcode_entry')
+            steam_guard_input_line.send_keys(STEAM_GUARD_CODE)
+            print('twofactorcode_entry - mobile login')
+
+        except Exception:
+            # Email authorizing
+            steam_guard_input_line = driver.find_element(By.ID, 'authcode')
+            steam_guard_input_line.send_keys(STEAM_GUARD_CODE)
+            print('authcode - email login')
+        import ipdb; ipdb.set_trace()
+        try:
+            # Mobile (SteamGuard) authorizing
+            driver.find_element(By.XPATH, '//*[@id="login_twofactorauth_buttonset_entercode"]/div[1]').click()
+            time.sleep(1.33)
+            if driver.current_url == 'https://store.steampowered.com/':
+                return True
+            return False
+
+        except Exception:
+            # Email authorizing
+            driver.find_element(By.XPATH, '//*[@id="auth_buttonset_entercode"]/div[1]').click()
+            time.sleep(1.33)
+            import ipdb;ipdb.set_trace()
+            try:
+                if driver.find_element(By.CLASS_NAME, 'auth_icon.auth_icon_unlock'):
+                    return True
+            except Exception:
+                return False
+
+
+
+    return print('bad authorization data')
--- data/config.py
+++ data/config.py
@@ -5,18 +5,45 @@ PATH_TO_WEBDRIVER = os.getcwd().replace('\\\\', '\\') + '\\chromedriver.exe'
 
 
 def get_data(USER_LOGIN=None, USER_PASSWORD=None):
-    with open('config.json', 'r') as file:
-        output = file.read()
-        object_json = json.loads(output)
+    try:
+        with open('config.json', 'r') as file:
+            output = file.read()
+            object_json = json.loads(output)
 
-    if USER_LOGIN:
+        if USER_LOGIN:
+            return object_json['userdata']['username']
+
+        elif not USER_PASSWORD:
+            return object_json['userdata']['password']
+    except Exception as err:
+        pass
+
+
+def get_user_login():
+    try:
+        with open('config.json', 'r') as file:
+            output = file.read()
+            object_json = json.loads(output)
         return object_json['userdata']['username']
+    except Exception as err:
+        print('Cant get user login')
 
-    elif USER_PASSWORD:
+
+def get_user_password():
+    try:
+        with open('config.json', 'r') as file:
+            output = file.read()
+            object_json = json.loads(output)
         return object_json['userdata']['password']
+    except Exception as err:
+        print('Cant get user password')
 
-
-USER_LOGIN = get_data(USER_LOGIN=True)
-USER_PASSWORD = get_data(USER_PASSWORD=True)
 
 STEAM_AUTHORIZATION_PAGE = 'https://store.steampowered.com/login/'
+
+MENU_CHOICE_1 = 'Starting program...'
+MENU_CHOICE_2 = 'Changing Steam account'
+MENU_CHOICE_3 = 'Changing nicknames set'
+MENU_CHOICE_4 = 'Reset config.json file'
+MENU_CHOICE_5 = 'Exiting program'
+MENU_CHOICE_6 = 'Error....EXIT PROGRAM'
