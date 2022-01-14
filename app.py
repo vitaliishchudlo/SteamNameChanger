028c51a35fd83eaf8e22bad9dee1efe8e1f4d42c
Vitalii <vitalik.shchudlo01@ukr.net>
2022-01-15 00:38:28 +0200

    New structure of the project

--- app.py
+++ app.py
@@ -1,7 +1,61 @@
+<<<<<<< HEAD
+import os
+
+from scripts import create_config_file, create_chrome_webdriver
+
+from selenium import webdriver
+
+from data import PLATFORM_RUNNING
+
+=======
+>>>>>>> e8fa894409b03ae5309175dfa6a8b1eecdcc8a7b
 def files_manager():
     """
     Checks for the required script files.
     """
+<<<<<<< HEAD
+    # Create config file with all configurations of the program
+    if not os.path.isfile('config.json'):
+        create_config_file()
+
+    #  Create chrome webdriver file
+    if PLATFORM_RUNNING == 'Linux':
+        if not os.path.isfile('chromedriver'):
+            create_chrome_webdriver()
+    else:
+        if not os.path.isfile('chromedriver.exe'):
+            create_chrome_webdriver()
+
+    # return all logs here
+
+def set_settings():
+    """
+    The function that is responsible for initialization the configurations from config file.
+    """
+    try:
+
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])  # Clear some errors in the terminal
+
+        # Option with hiding window
+
+        #  Creating driver object and returning it
+        driver = webdriver.Chrome(options=options)
+        # ADD ALSO PATH TO WEBDRIVER
+
+        return driver
+    except Exception as error:
+        print('Error')
+
+
+def get_menu():
+    """
+     The function that is responsible for main menu of the program.
+    """
+    pass
+
+
+=======
     pass
 
 
@@ -19,16 +73,25 @@ def get_menu():
     pass
 
 
+>>>>>>> e8fa894409b03ae5309175dfa6a8b1eecdcc8a7b
 def app():
     """
     The main function, which responsible for starting the program and all managing it.
     """
     try:
+        driver = files_manager()
         set_settings()
+<<<<<<< HEAD
+        get_menu()
+    except Exception as err:
+        print(f'[Error]: There were some errors when starting the program.\n[Reason]: {err}')
+        os.remove('config.json')
+=======
 
         get_menu()
     except Exception as err:
         print(f'[Error]: There were some errors when starting the program.\n[Reason]: {err}')
+>>>>>>> e8fa894409b03ae5309175dfa6a8b1eecdcc8a7b
 
 
 if __name__ == '__main__':
--- data/config.py
+++ data/config.py
@@ -1,9 +1,43 @@
+<<<<<<< HEAD
+import platform
+import os
+=======
+>>>>>>> e8fa894409b03ae5309175dfa6a8b1eecdcc8a7b
 # Name of the program
 # Version of the program
 # Author
 
+<<<<<<< HEAD
+# Platform the program running on
+PLATFORM_RUNNING = platform.system()
+
+# WebDriver
+PATH_TO_WEBDRIVER = os.getcwd().replace('\\\\', '\\') + '\\chromedriver.exe'
+PATH_TO_WEBDRIVER_LINUX = os.getcwd() + '/chromedriver'
+=======
+>>>>>>> e8fa894409b03ae5309175dfa6a8b1eecdcc8a7b
 
 # Links on pages:
 
 
+<<<<<<< HEAD
+# Menu chooses:
+
+
+# Config.json file
+CONFIG_STRUCTURE_DICT = {
+    "userdata": {
+        "username": "",
+        "password": ""
+    },
+    "nickNamesManagement": {
+        "nickNamesForChange": [],
+        "lastNickName": None
+    },
+    "chromeWindowSettings": {
+        "hideWindow": False
+    }
+}
+=======
 # Menu choises
+>>>>>>> e8fa894409b03ae5309175dfa6a8b1eecdcc8a7b
